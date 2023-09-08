7ecda0b77214263b2a57b457a3b1c30cd140d946
Valera <User@WIN-8BKRLOA2SS8>
2023-09-08 23:08:40 +0200

    Добавьте файлы проекта.

---
+++ DZ/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Домашняя работа ");
+try
+{
+
+
+    Console.WriteLine("Введите n:");
+    double n = double.Parse(Console.ReadLine());
+    double a = n % 10;
+    double b = n / 100 % 10;
+    double c = n / 1000 / 100;
+    double r = n / 1000;
+    Console.WriteLine("Сумма цифр введенного числа равна:");
+    Console.WriteLine(a + b + c + r);
+    Console.WriteLine("Произведение цифр введенного числа равна:");
+    Console.WriteLine(a * b * c * r);
+
+}
+catch
+{
+    Console.WriteLine("Введите верное значение:");
+}
