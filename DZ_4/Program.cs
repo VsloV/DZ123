ee3527edbae5124dd6bd49076c82eb25170e9135
Valera <User@WIN-8BKRLOA2SS8>
2023-09-21 18:53:58 +0200

    Last

--- DZ_4/Program.cs
+++ DZ_4/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+string a = "28";
+byte b = byte.Parse(a);
+uint c = uint.Parse(a);
+int d = int.Parse(a);
+long e = long.Parse(a);
+double f = double.Parse(a);
+Console.WriteLine(f);
