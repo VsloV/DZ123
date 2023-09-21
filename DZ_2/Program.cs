6540971cf91e5c4b37bcfe566bc4f14abfd4f741
Valera <User@WIN-8BKRLOA2SS8>
2023-09-21 17:32:24 +0200

    1

--- DZ_2/Program.cs
+++ DZ_2/Program.cs
@@ -2,38 +2,38 @@
 Console.WriteLine("Используя константы,напишите программу для перевода миллиметров в:A)сантиметры B)метры C)километры D)миля E)ярды");
 
 //A
-//миллиметры в сантиметры
-//const float sm = 0.01f;
-//Console.Write("Введите миллиметры:");
-//double mm = double.Parse(Console.ReadLine());
-//double res = sm * mm;
-//Console.WriteLine($"{mm} миллиметры {res:F2} сантиметров");
+миллиметры в сантиметры
+const float sm = 0.01f;
+Console.Write("Введите миллиметры:");
+double mm = double.Parse(Console.ReadLine());
+double res = sm * mm;
+Console.WriteLine($"{mm} миллиметры {res:F2} сантиметров");
 
 //B
-//////миллиметр в метры
-//const float m = 0.001f;
-//Console.WriteLine("Введите милиметры:");
-//double mm = double.Parse(Console.ReadLine());
-//double res = m * mm;
-//Console.WriteLine($"{m} милиметров {res:F2} метров");
+////миллиметр в метры
+const float m = 0.001f;
+Console.WriteLine("Введите милиметры:");
+double mm = double.Parse(Console.ReadLine());
+double res = m * mm;
+Console.WriteLine($"{m} милиметров {res:F2} метров");
 
 //C
-//////миллиметры в километры 
-//const float km = 0.000001f;
-//Console.WriteLine("Введите милиметры:");
-//double mm = double.Parse(Console.ReadLine());
-//double sum = km * mm;
-//Console.WriteLine($"{mm} милиметров {sum} километров");
+////миллиметры в километры 
+const float km = 0.000001f;
+Console.WriteLine("Введите милиметры:");
+double mm = double.Parse(Console.ReadLine());
+double sum = km * mm;
+Console.WriteLine($"{mm} милиметров {sum} километров");
 
 //D
-//////миллиметры в мили
-//Console.WriteLine("Введите милиметры:");
-//double mm = double.Parse(Console.ReadLine());
-//double res = mm / 1609344;
-//Console.WriteLine($"{mm} мили {res:F2} мили");
+//миллиметры в мили
+Console.WriteLine("Введите милиметры:");
+double mm = double.Parse(Console.ReadLine());
+double res = mm / 1609344;
+Console.WriteLine($"{mm} милиметров {res:F2} мили");
 
 //E
-//////миллиметр в ярды
+////миллиметр в ярды
 const float yard = 0.0011f;
 Console.Write("Введите миллиметры:");
 double mm = double.Parse(Console.ReadLine());
