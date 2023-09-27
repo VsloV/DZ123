47c7fcf7759b6bdaa97d4bb8bb76b6b4e018e50d
Valera <User@WIN-8BKRLOA2SS8>
2023-09-27 15:49:38 +0200

    Last

---
+++ Lesson2 topic2/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//1
+Console.WriteLine("Two roads diverged in a yellow wood,\r\nAnd sorry I could not travel both\r\nAnd be one traveler, long I stood\r\nAnd looked down one as far as I could\r\nTo where it bent in the undergrowth. ");
+
+int firsNumber = 1;
+int seconNumber = 13;
+int thirdNimber = 49;
+Console.WriteLine($"{firsNumber} {seconNumber} {thirdNimber}");
+
+int fourthNumber = 63;
+int fifteenNumber = 77;
+int sixteenNumber = 92;
+Console.WriteLine($"{fourthNumber}  {fifteenNumber}  {sixteenNumber}");
+
+
