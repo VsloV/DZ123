1ec8e10278123cfee33294c97decb428599e01a8
Valera <User@WIN-8BKRLOA2SS8>
2023-09-28 16:35:07 +0200

    Last

---
+++ Topic3 lesson2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string FirstSentence = "Добиться успеха легко";
+string SecondSentence = " познать смысл - вот проблема";
+string Result = FirstSentence + SecondSentence;
+Console.WriteLine($"{Result}");
